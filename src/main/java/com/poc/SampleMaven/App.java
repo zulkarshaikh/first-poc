9c6d86bbfa0afa9e58f4faddba966858aeb36215
zulfi <zulkar.shaikh@gmail.com>
2019-10-11 23:58:29 +0100

    adding files

---
+++ src/main/java/com/poc/SampleMaven/App.java
@@ -0,0 +1,13 @@
+package com.poc.SampleMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
